a95bbb5bfdd6660bd33663c1fafa500226d1a802
eduganau <eganau@fihoca.com>
2020-12-28 13:35:18 +0100

    list with mock entries

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarComponentOptions } from 'ion2-calendar';
+import { CalendarComponentOptions, DayConfig } from 'ion2-calendar';
+import * as moment from 'moment';
+import { DayEntry } from 'src/app/interfaces/DayEntry';
 
 @Component({
   selector: 'app-calendar',
@@ -14,15 +16,47 @@ export class CalendarComponent implements OnInit {
     color: 'secondary',
     weekStart: 1
   }
+  daysConfig: DayConfig[] = [];
 
-  constructor() { }
+  dayEntries: DayEntry[];
 
-  ngOnInit() {}
+  constructor() { 
+    this.daysConfig.push({
+      date: new Date(1609196400000),
+      subTitle: "\u2713",
+      cssClass: 'filledDay'
+    })
+    this.options.daysConfig = this.daysConfig;
+
+    this.dayEntries = [{
+        date: '27/12/2020',
+        textEntry: 'Avui ha fet molt bon dia'
+      },
+      {
+        date: '28/12/2020',
+        textEntry: 'Avui ha fet aire'
+      },
+      {
+        date: '29/12/2020',
+        textEntry: 'Dema fará dia per anar amb bus'
+      }
+    ].sort((a, b) => (a.date < b.date) ? 1 : -1) // parsejar les strings a Dates i comparar-les
+  }
+
+  ngOnInit() {
+    
+  }
 
   onSelect(event: any) {
-    var parsedDate = new Date(event.time).toLocaleDateString();
+    var parsedDate = new Date(event.time).toLocaleDateString(); // selected Date from calendar
+    var parsedTime = new Date().toLocaleString(); // current time 
     console.log(event);
     console.log(parsedDate)
+    console.log(parsedTime)
+
+    console.log(moment(parsedDate, "DD/MM/YYYY").format('dddd DD'))
   }
+
+  
 
 }
